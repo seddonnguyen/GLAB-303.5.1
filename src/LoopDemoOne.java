17ce4aa26684b8138d6a53d9f59596b4f72e4b28
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-03-18 22:30:07 -0600

    Add project files

---
+++ src/LoopDemoOne.java
@@ -0,0 +1,11 @@
+public class LoopDemoOne {
+    public static void main(String[] args) {
+        // Example 1: Display a Text Five Times
+        int n = 5;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            System.out.println("Java is fun");
+        }
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Program to find the sum of natural numbers from 1 to 1000.
+public class Main {
+    public static void main(String[] args) {
+        // Example 2: Display Sum of n Natural Numbers
+        int sum = 0;
+        int n = 1000;
+        // for loop
+        for (int i = 1; i <= n; ++i) {
+            // body inside for loop
+            sum += i;     // sum = sum + i
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
